ffd8e8c3c503fc84bd9e7cd6265879ddd41e88fa
Alex Saber <alexsaber89@gmail.com>
2017-06-05 21:00:40 -0500

    add initial authorization routes

--- SalesProductivityTracker.App/app/app.js
+++ SalesProductivityTracker.App/app/app.js
@@ -1,1 +1,33 @@
 ﻿var app = angular.module("SalesProductivityTracker", ["ngRoute"]);
+
+app.config([
+    "$routeProvider", function ($routeProvider) {
+        $routeProvider
+            .when("/",
+            {
+                templateUrl: "app/partials/Login.html",
+                controller: "loginController"
+            })
+            .when("/home",
+            {
+                templateUrl: "app/partials/EmployeeHome-ProductivityLog.html",
+                controller: "productivityLogController"
+            })
+            .when("/register",
+            {
+                templateUrl: "app/partials/Register.html",
+                controller: "registerController"
+            });
+
+    }
+]);
+
+app.run(["$http", function ($http) {
+
+    var token = sessionStorage.getItem('token');
+
+    if (token)
+        $http.defaults.headers.common['Authorization'] = `bearer ${token}`;
+
+}
+]);
