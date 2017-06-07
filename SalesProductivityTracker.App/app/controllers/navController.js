b71534eb5fd9817e461dcb8235d8fced3b8f9338
Alex Saber <alexsaber89@gmail.com>
2017-06-06 20:21:49 -0500

    add navController with placeholder logic, to test functionality

---
+++ SalesProductivityTracker.App/app/controllers/navController.js
@@ -0,0 +1,15 @@
+﻿app.controller("navController", ["$scope", "$location", function ($scope, $location) {
+
+    $scope.showLogin = () => {
+        $location.url('/');
+    };
+
+    $scope.showRegister = () => {
+        $location.url('/register');
+    };
+
+    $scope.showHome = () => {
+        $location.url('/home');
+    };
+
+}]);
