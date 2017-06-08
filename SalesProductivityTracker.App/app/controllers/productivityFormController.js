8b842861f6e8f251b98f01cfaa7f5f8d292fc786
Alex Saber <alexsaber89@gmail.com>
2017-06-08 09:37:34 -0500

    add the rest of the Productivity Form fields; add timestamp in controller

--- SalesProductivityTracker.App/app/controllers/productivityFormController.js
+++ SalesProductivityTracker.App/app/controllers/productivityFormController.js
@@ -3,8 +3,13 @@
     $scope.submitProductivityReport = submitProductivityReport;
 
     function submitProductivityReport(productivityForm) {
+        var timestamp = new Date().toString();
+        console.log("timestamp: ", timestamp);
         console.log("productivityDate: ", productivityForm.productivityDate);
         console.log("bookedDailyRevenue: ", productivityForm.bookedDailyRevenue);
+        console.log("dailyCasesCompleted: ", productivityForm.dailyCasesCompleted);
+        console.log("dailyCallCount: ", productivityForm.dailyCallCount);
+        console.log("dailyEmailCount: ", productivityForm.dailyEmailCount);
     }
 
 }]);
