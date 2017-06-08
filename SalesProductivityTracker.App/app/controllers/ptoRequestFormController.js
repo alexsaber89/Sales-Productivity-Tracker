a4044852a491686622d7591e3cfa2df8ce2f7bc1
Alex Saber <alexsaber89@gmail.com>
2017-06-08 10:03:38 -0500

    add PTO Request Form partial and placeholder controller logic

--- SalesProductivityTracker.App/app/controllers/ptoRequestFormController.js
+++ SalesProductivityTracker.App/app/controllers/ptoRequestFormController.js
@@ -1,5 +1,13 @@
 ﻿app.controller("ptoRequestFormController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
 
+    $scope.submitPTORequestForm = submitPTORequestForm;
 
+    function submitPTORequestForm(ptoRequestForm) {
+        var timestamp = new Date().toString();
+        console.log("timestamp: ", timestamp);
+        console.log("requestedPTODate: ", ptoRequestForm.requestedPTODate);
+        console.log("ptoType: ", ptoRequestForm.ptoType);
+        console.log("notes: ", ptoRequestForm.notes);
+    }
 
 }]);
