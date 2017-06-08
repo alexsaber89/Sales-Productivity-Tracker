f25806312d0ec024465f9a8789171fd2b86918e1
Alex Saber <alexsaber89@gmail.com>
2017-06-07 23:48:50 -0500

    add productivityFormController test logic

--- SalesProductivityTracker.App/app/controllers/productivityFormController.js
+++ SalesProductivityTracker.App/app/controllers/productivityFormController.js
@@ -1,5 +1,10 @@
 ﻿app.controller("productivityFormController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
 
-    
+    $scope.submitProductivityReport = submitProductivityReport;
+
+    function submitProductivityReport(productivityForm) {
+        console.log("productivityDate: ", productivityForm.productivityDate);
+        console.log("bookedDailyRevenue: ", productivityForm.bookedDailyRevenue);
+    }
 
 }]);
