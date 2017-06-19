96679f6eee020d6f72d95202fdf3a914d983bc1d
Alex Saber <alexsaber89@gmail.com>
2017-06-18 23:42:49 -0500

    refactor Employee Navbar into custom directive

---
+++ SalesProductivityTracker.App/app/directives/employeeNavbar.js
@@ -0,0 +1,29 @@
+﻿angular.module("app.directives.employeeNavbar", [])
+    .directive("employeeNavbar", function () {
+        return {
+            restrict: "E",
+            //scope: {
+            //    productivityForms: "<"
+            //},
+            templateUrl: "app/templates/employeeNavbar.html",
+            controller: function ($scope, $location) {
+
+                $scope.logout = () => {
+                    $location.url('/');
+                };
+
+                $scope.showHome = () => {
+                    $location.url('/home');
+                };
+
+                $scope.showProductivityForm = () => {
+                    $location.url('productivityform');
+                };
+
+                $scope.showPTORequestForm = () => {
+                    $location.url('/requestpto');
+                };
+
+            }
+        };
+    });
