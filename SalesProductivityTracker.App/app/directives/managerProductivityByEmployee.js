31a477348be89bac7758f8429e768f22449b690e
Alex Saber <alexsaber89@gmail.com>
2017-06-21 15:58:35 -0500

    formatForms function now working

--- SalesProductivityTracker.App/app/directives/managerProductivityByEmployee.js
+++ SalesProductivityTracker.App/app/directives/managerProductivityByEmployee.js
@@ -9,9 +9,8 @@
             controller: function ($scope, $location, managerProductivityByEmployeeFactory) {
 
                 $scope.getAllProductivityForms = getAllProductivityForms;
+                $scope.formatForms = formatForms;
                 $scope.setProductivityQuarter = setProductivityQuarter;
-                $scope.formatProductivityForms = formatProductivityForms;
-
                 $scope.quarters = [{
                     quarter: 1
                 }, {
@@ -23,95 +22,136 @@
                 }];
 
                 $scope.quarter = $scope.quarters[0].quarter;
-                console.log("$scope.quarter: ", $scope.quarter);
+
+                function setProductivityQuarter(quarterNum) {
+                    $scope.quarter = quarterNum.quarter;
+                    formatForms($scope.unformattedForms, $scope.quarter)
+                };
 
                 getAllProductivityForms();
 
                 function getAllProductivityForms() {
                     managerProductivityByEmployeeFactory.getAllProductivityForms().then(function (forms) {
-                        $scope.unformattedProductivityForms = forms;
-                        formatProductivityForms();
+                        $scope.unformattedForms = forms;
+                        formatForms(forms, 1);
                         $location.url('/manager-home');
                     });
                 };
 
-                function setProductivityQuarter(quarterNum) {
-                    $scope.quarter = quarterNum.quarter;
-                    console.log("$scope.quarter: ", $scope.quarter);
-                };
+                function formatForms(forms, quarter) {
 
+                    let productivityTableObjects = [];
 
-                function formatProductivityForms() {
-                    //let evaluatedMetric = 0;
-                    let formattedProductivityForms = [];
-
-                    let userIdArray = $scope.unformattedProductivityForms.map(function (form) {
+                    // Extract all userId's into an array
+                    let userIdArray = forms.map(function (form) {
                         return form.User.Id;
                     });
 
+                    // Remove duplicate userId's from previous array
                     let uniqueUserIdArray = userIdArray.filter(function (elem, index, self) {
                         return index == self.indexOf(elem);
                     });
 
-                    console.log("uniqueUserIdArray: ", uniqueUserIdArray);
-
-                    $scope.quarterlyForms = $scope.unformattedProductivityForms.filter(function (form) {
-                        return form.Quarter === $scope.quarter;
+                    uniqueUserIdArray.forEach(function (userId) {
+                        let employee = {};
+                        employee.UserId = userId;
+                        employee.QuarterlyCallCount = getMetricsForEmployeeByQuarter(forms, userId, "DailyCallCount", quarter);
+                        employee.QuarterlyEmailCount = getMetricsForEmployeeByQuarter(forms, userId, "DailyEmailCount", quarter);
+                        employee.QuarterlyCasesCompleted = getMetricsForEmployeeByQuarter(forms, userId, "DailyCasesCompleted", quarter);
+                        employee.BookedQuarterlyRevenue = getMetricsForEmployeeByQuarter(forms, userId, "BookedDailyRevenue", quarter);
+                        productivityTableObjects.push(employee);
                     });
 
-                    console.log("$scope.quarterlyForms: ", $scope.quarterlyForms);
-
-                    function viewObjectCreator(userArray, formsArray) {
-                        let viewObject = {};
-                        let DailyCallCount = 0;
-                        let DailyEmailCount = 0;
-                        let DailyCasesCompleted = 0;
-                        let BookedDailyRevenue = 0;
-
-                        userArray.forEach(function (id) {
-                            formsArray.forEach(function (form) {
-                                if (id === form.User.Id) {
-                                    viewObject.UserId = id;
-                                    DailyCallCount += form.DailyCallCount;
-                                    DailyEmailCount += form.DailyEmailCount;
-                                    DailyCasesCompleted += form.DailyCasesCompleted;
-                                    BookedDailyRevenue += form.BookedDailyRevenue;
-                                }
-                            });
-                        });
-
-                        viewObject.DailyCallCount = DailyCallCount;
-                        viewObject.DailyEmailCount = DailyEmailCount;
-                        viewObject.DailyCasesCompleted = DailyCasesCompleted;
-                        viewObject.BookedDailyRevenue = BookedDailyRevenue;
-                        formattedProductivityForms.push(viewObject);
-                    };
-
-                    viewObjectCreator(uniqueUserIdArray, $scope.quarterlyForms);
-
-                    console.log("formattedProductivityForms: ", formattedProductivityForms);
-
-
-                    //switch (metric) {
-                    //    case "DailyCallCount":
-                    //        var DailyCallCount = evaluatedMetric;
-                    //        return DailyCallCount;
-                    //        break;
-                    //    case "DailyEmailCount":
-                    //        var DailyEmailCount = evaluatedMetric;
-                    //        return DailyEmailCount;
-                    //        break;
-                    //    case "DailyCasesCompleted":
-                    //        var DailyCasesCompleted = evaluatedMetric;
-                    //        return DailyCasesCompleted;
-                    //        break;
-                    //    case "BookedDailyRevenue":
-                    //        var BookedDailyRevenue = evaluatedMetric;
-                    //        return BookedDailyRevenue;
-                    //        break;
-                    //}
+                    console.log("productivityTableObjects: ", productivityTableObjects);
 
                 };
+
+                const getMetricsForEmployeeByQuarter = function (forms, employeeId, metric, quarter) {
+
+                    const quarterReportsForEmployee = forms.filter(function (report) {
+                        return report.User.Id === employeeId && report.Quarter === quarter;
+                    });
+
+                    const metricSum = quarterReportsForEmployee.reduce(function (acc, obj) {
+                        return acc + obj[metric];
+                    }, 0);
+
+                    return metricSum;
+                };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //function formatProductivityForms(quarter) {
+
+                //    let desiredQuarter;
+
+                //    if (quarter === 0) {
+                //        desiredQuarter = 1;
+                //    } else {
+                //        desiredQuarter = quarter;
+                //    }
+
+                //    let formattedProductivityForms = [];
+
+                //    // Extract all userId's into an array
+                //    let userIdArray = $scope.unformattedProductivityForms.map(function (form) {
+                //        return form.User.Id;
+                //    });
+
+                //    // Remove duplicate userId's from previous array
+                //    let uniqueUserIdArray = userIdArray.filter(function (elem, index, self) {
+                //        return index == self.indexOf(elem);
+                //    });
+
+                //    // Extract desired quarter's forms into an array
+                //    let quarterlyForms = $scope.unformattedProductivityForms.filter(function (form) {
+                //        return form.Quarter === desiredQuarter;
+                //    });
+
+                //    // For each userId, add up metrics and return final user object
+                //    let viewObjArray = [];
+
+                //    for (let i = 0; i < uniqueUserIdArray.length; i++) {
+                //        let viewObj = viewObjectCreator(uniqueUserIdArray[0], quarterlyForms);
+                //        viewObjArray.push(viewObj);
+                //    };
+
+                //    function viewObjectCreator(userId, formsArray) {
+                //        let viewObject = {};
+                //        let DailyCallCount = 0;
+                //        let DailyEmailCount = 0;
+                //        let DailyCasesCompleted = 0;
+                //        let BookedDailyRevenue = 0;
+
+                //        formsArray.forEach(function (form) {
+                //            if (userId === form.User.Id) {
+                //                DailyCallCount += form.DailyCallCount;
+                //                DailyEmailCount += form.DailyEmailCount;
+                //                DailyCasesCompleted += form.DailyCasesCompleted;
+                //                BookedDailyRevenue += form.BookedDailyRevenue;
+                //            }
+                //        });
+
+                //        viewObject.DailyCallCount = DailyCallCount;
+                //        viewObject.DailyEmailCount = DailyEmailCount;
+                //        viewObject.DailyCasesCompleted = DailyCasesCompleted;
+                //        viewObject.BookedDailyRevenue = BookedDailyRevenue;
+                //        return viewObject;
+                //    };
+
+                //    console.log("formatProductivityForms viewObjArray: ", viewObjArray);
+                //};
             }
         };
     });
